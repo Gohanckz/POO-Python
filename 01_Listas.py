2233440f4bddee9d8b2d84306eb2a9a7856975ac
G0H4NCKZ <ing.ivan.rodrigo@gmail.com>
2018-09-21 19:07:58 -0300

    ejercicio de desicion logica simple

--- 01_Listas.py
+++ 01_Listas.py
@@ -1,3 +1,7 @@
 lista = ["Ivan",24,["Hola","Mundo",1994]]
 
 print(lista[1])
+
+#Sobreescribir elementos
+
+lista[2,1] = "Reemplazo"
---
+++ 02_Desicion_logica.py
@@ -0,0 +1,7 @@
+#peticion de datos
+libros = int(input("Cuantos libros lees anualmente?"))
+#condiciones
+if libros>=15:
+	print("Eres un buen lector")
+else:
+	print("Necesitas leer más")
